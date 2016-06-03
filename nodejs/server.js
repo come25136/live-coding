c19660a6bda922f0a48ef44a70ea938978f5047b
0918nobita <nobita.0918@gmail.com>
2016-06-03 15:27:49 +0900

    requireでejs読み込み

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,5 +1,6 @@
-var http = require("http");
-var fs = require("fs");
+var http = require("http"),
+    fs = require("fs"),
+    ejs = require("ejs");
 
 http.createServer(function(req, res){
     res.writeHead(200, {"Content-Type" : "text/html"});
